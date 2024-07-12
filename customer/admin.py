d9c60a058d10dc514d7aefd01a2944613ad017c2
sarvar <sarvar7084295@mail.ru>
2024-07-12 07:22:51 +0500

    Excel export, sortable done

--- customer/admin.py
+++ customer/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 
 from customer.forms import UserModelForm
 from customer.models import Customer, User
+from adminsortable2.admin import SortableAdminMixin
 
 
 # Register your models here.
 # admin.site.register(Customer)
 
 @admin.register(Customer)
-class CustomerModelAdmin(admin.ModelAdmin):
+class CustomerModelAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ['id', 'full_name', 'email', 'is_active']
     search_fields = ['email', 'id']
     list_filter = ['joined', 'is_active']
+    ordering = ('-joined', 'order')
 
     def has_add_permission(self, request):
         return True
